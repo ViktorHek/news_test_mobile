1080d5263e0ac77c47fd1346c6c6808d2d75a149
viktor <viktorkarlsson125@gmail.com>
2021-01-13 16:20:52 +0100

    adding external API

--- state/reducers/rootReducer.js
+++ state/reducers/rootReducer.js
@@ -1,5 +1,16 @@
-const rootReducer = (state, action) => {
-  return state
-}
 
-export default rootReducer
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_ARTICLE_INDEX":
+      return {
+        ...state,
+        articles: action.payload.articles
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
